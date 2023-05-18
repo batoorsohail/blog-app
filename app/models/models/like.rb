5171a441a97cedb3548c4aa7a058b9309198108a
unknown <sohailshahidi1435@gmail.com>
2023-05-18 17:49:38 +0430

    Add optional suggestions

--- app/models/models/like.rb
+++ app/models/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :update_post_likes_counter
+
+  private
+
   def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+    post.increment!(:likes_counter)
   end
 end
--- app/models/models/post.rb
+++ app/models/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def update_author_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
+  after_create :update_author_posts_counter
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_author_posts_counter
+    author.increment!(:posts_counter)
   end
 end
