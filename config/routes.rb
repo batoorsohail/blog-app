e1c0138f20f4cfff8a12e36484dad9bfc54850bd
unknown <sohailshahidi1435@gmail.com>
2023-05-23 14:10:42 +0430

    Add show function in posts_controller.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  # resources :users, only: [:index]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: %i[index show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
