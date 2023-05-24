aae93b7fe4bfdf063fe1cc59991905cf1279555e
unknown <sohailshahidi1435@gmail.com>
2023-05-24 16:32:29 +0430

    Add tests for posts_controller.rb file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe "Posts", type: :request do
       expect(response.body).to include("List of all posts")
     end
   end
+
+  describe "Get specifc user's post" do
+    user = User.create!(name: 'Ahmad', photo: 'www.image.com', bio: 'OG', posts_counter: 3)
+    post = Post.create!(title: 'Spec Test', author_id: user.id, comments_counter: 5, likes_counter: 10)
+
+    it "returns correct status" do
+      get user_posts_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it "return the correct placeholder" do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(response.body).to include("Here are the details of a specific post")
+    end
+  end
 end
