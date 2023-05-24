024c928852b831cd1217f24589d79640f4770011
unknown <sohailshahidi1435@gmail.com>
2023-05-24 16:18:55 +0430

    Add tests for users_controller.rb file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include("List of all users")
     end
   end
+
+  describe "Get specifc user" do
+    user = User.create!(name: 'Ahmad', photo: 'www.image.com', bio: 'OG', posts_counter: 3)
+
+    it "returns correct status" do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it "return the correct placeholder" do
+      get "/users/#{user.id}"
+      expect(response.body).to include("Shows user with id")
+    end
+  end
 end
