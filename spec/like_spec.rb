8eeb8f063ce70b4500407f37799f5aa6b7dbce2e
unknown <sohailshahidi1435@gmail.com>
2023-05-30 17:33:39 +0430

    Add tests for user's view

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,26 +1,107 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  like = Like.new(author_id: 1, post_id: 1)
-
-  it 'Reutrns author id' do
-    expect(like.author_id).to eql 1
+RSpec.describe 'users/index', type: :feature do
+  before(:each) do
+    @user = [
+      User.create(
+        name: 'Mustafa',
+        photo: 'https://pic.com',
+        bio: 'Software Engineer Turkey',
+        posts_counter: 2
+      ),
+      User.create(
+        name: 'Mert',
+        photo: 'https://pic.com',
+        bio: 'Software Engineer from Turkey',
+        posts_counter: 3
+      ),
+      User.create(
+        name: 'Caney',
+        photo: 'https://pic.com',
+        bio: 'Software Engineer from England',
+        posts_counter: 3
+      )
+    ]
+    visit users_path
   end
 
-  it 'Reutrns post id' do
-    expect(like.post_id).to eql 1
+  it 'shows the author image' do
+    expect(page).to have_css('img')
   end
 
-  it 'checks likes_counter method' do
-    User.new(name: 'Ahmad', photo: 'www.iamge.com', bio: 'Full Stack Developer', posts_counter: 5)
-    post = Post.new(author_id: 1, title: 'New', text: 'First post', comments_counter: 3, likes_counter: 2)
-    like = Like.new(author_id: 1, post:)
+  it 'shows the username of all users' do
+    @user.each do |user|
+      expect(page).to have_content(user.name)
+    end
+  end
 
-    post.likes_counter = 2
-    post.save
+  it 'shows the number of posts by each author' do
+    @user.each do |user|
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+    end
+  end
 
-    like.update_post_likes_counter
-    like.update_post_likes_counter
-    expect(post.likes_counter).to eq(4)
+  it 'redirects to users show page when clicked' do
+    @user.each do |user|
+      click_link user.name
+      expect(page).to have_current_path(user_path(user))
+      visit users_path
+    end
+  end
+  describe 'users/show', type: :feature do
+    before(:each) do
+      @user = [
+        User.create(name: 'Mustafa', photo: 'https://pic.com', bio: 'Software Engineer Turkey',
+                    posts_counter: 2),
+        User.create(name: 'Mert', photo: 'https://pic.com', bio: 'Software Engineer from England',
+                    posts_counter: 3)
+      ]
+
+      @first_user = User.first
+      @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post from Mert',
+                                comments_counter: 1, likes_counter: 1)
+      @second_post = Post.create(author: @first_user, title: 'How are you?', text: 'This is my second post from Mert',
+                                 comments_counter: 0, likes_counter: 1)
+      @third_post = Post.create(author: @first_user, title: 'How are you?', text: 'This is my third post from Mert',
+                                comments_counter: 0, likes_counter: 1)
+      visit user_path(@first_user)
+    end
+
+    it 'shows the author name' do
+      expect(page).to have_content(@first_user.name)
+    end
+
+    it 'shows the author image' do
+      expect(page).to have_css('img')
+    end
+
+    it 'shows the user bio' do
+      expect(page).to have_content(@first_user.bio)
+    end
+
+    it 'shows number of posts by the author' do
+      expect(page).to have_content(@first_user.posts_counter)
+    end
+
+    it 'shows the users first three posts' do
+      expect(page).to have_content(@first_post.text)
+      expect(page).to have_content(@second_post.text)
+      expect(page).to have_content(@third_post.text)
+    end
+
+    it 'shows button to view all posts' do
+      expect(page).to have_link('See all posts')
+    end
+
+    it 'redirects to posts show page when users post clicked' do
+      visit user_posts_path(@first_user)
+      click_link @first_post.title
+      expect(current_path).to match(user_posts_path(@first_user.id))
+    end
+
+    it 'it redirects me to the users posts index page when i clicked see all posts' do
+      click_link 'See all posts'
+      expect(current_path).to match(user_posts_path(@first_user))
+    end
   end
 end
